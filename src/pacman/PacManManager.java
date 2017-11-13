fb641c71b044ce0d4aba3abfe12a71e6fde218ce
Thinker222 <es0197@yahoo.com>
2017-11-12 22:43:55 -0500

    Added hooks for Pacman
    .

--- src/pacman/PacManManager.java
+++ src/pacman/PacManManager.java
@@ -1,9 +1,37 @@
-package pacman; 
+package pacman;
 
+
+import java.util.ArrayList;
 
 // Decision Making System
-public class PacManManager {
+public class PacManManager
+{
 private PacManFileManager pacManFileManager;
+
+// Variables
+private MazeData mazeData;
+Ghost[] ghosts;
+
+int action()
+{
+    // Take in weights
+
+
+    // Traverse Neuron Tree
+
+
+    // Find best fit, and if there is not a best-fit, create one.
+
+
+    // Take the action
+
+
+    // Record Result of Action and update result in Neuron Tree
+
+
+
+    return 1;
+}
 
 
 
@@ -16,3 +44,6 @@ class PacManFileManager
 {
 
 }
+
+
+
