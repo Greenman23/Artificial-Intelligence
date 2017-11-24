b8bb61cd6135ba06401286e811485eb022cb8523
Jcwolves <32345098+Jcwolves@users.noreply.github.com>
2017-11-24 15:17:06 -0500

    Revert "Neural Network kinda complete."
    This reverts commit a0224d70204d7e589569e05c4174e38d885701a3.

--- src/pacman/NeuralNetwork.java
+++ src/pacman/NeuralNetwork.java
@@ -1,7 +1,11 @@
 package pacman;
 
+import java.io.*;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.System.out;
 
 public class NeuralNetwork {
     private int depth;
@@ -42,13 +46,52 @@ public class NeuralNetwork {
 
     // For Jade, find a way to store and retrieve the neural network from a notepad file as efficiently as possible.
 // It would be best if only the bottom elements were stored in the notepad file, but I will leave that up to you. 
-    void storeCurrentTree() {
+    void storeCurrentTree()
+    {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("History.txt"))) {
+            bw.write(depth + " " + historySize);
+            bw.newLine();
+            String currentValue = "";
+            for (int i = 0; i > tree.length; i++){
+                currentValue = String.valueOf(tree[i]);
+                bw.write(currentValue);
+                bw.newLine();
+            }
+            bw.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
 
     }
 
 
     NeuralNetwork getTree(String fileName) {
-    return new NeuralNetwork(2,2);
+        File file = new File(fileName);
+        try (Scanner scan = new Scanner(file)){
+            int count = 0;
+            if (scan.hasNextInt())
+                depth = scan.nextInt();
+            else depth = 7;         //Dummy values
+            tree = new double[((int)Math.pow(2, depth))]; //Create our tree
+            if(scan.hasNextInt())
+                historySize = scan.nextInt();
+            else historySize = 14;  //Dummy values
+
+            while (scan.hasNextDouble() && count < tree.length){
+               tree[count] = scan.nextDouble();
+               count++;
+            }
+            scan.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File " + fileName + "not found");
+        }
+
+        //NeuralNetwork x = new NeuralNetwork();
+        return new NeuralNetwork(2, 2);
+
     }
+
+
 
 }
